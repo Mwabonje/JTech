699a2af83951b660290a7e31ed3a2067b307b2b1
mwabonje <ringa.michael@gmail.com>
2024-02-16 00:08:46 +0300

    Added testimonial section

---
+++ script.js
@@ -0,0 +1,12 @@
+
+  
+      // function to display a greeting alert
+    //   function displayGreeting() {
+    //     alert(
+    //       "Hey there! Your experience matters to us. Explore our website and tell us how we can make it better for you."
+    //     );
+    //   }
+
+      //When the window loads
+    //   window.onload=displayGreeting();
+   
