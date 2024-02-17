6e0444667f08a336a44af3768fa92764058c0441
mwabonje <ringa.michael@gmail.com>
2024-02-18 02:23:39 +0300

    Added some js for smooth scroll and also changed the background image

--- script.js
+++ script.js
@@ -10,3 +10,18 @@
       //When the window loads
     //   window.onload=displayGreeting();
    
+   //smooth scrolling behavior when anchor links are clicked
+    document.addEventListener("DOMContentLoaded", function() {
+      const anchorLinks = document.querySelectorAll('a[href^="#"]');
+      anchorLinks.forEach(anchor => {
+        anchor.addEventListener("click", function(e) {
+          e.preventDefault();
+          const targetId = this.getAttribute("href").substring(1);
+          const targetElement = document.getElementById(targetId);
+          if (targetElement) {
+            targetElement.scrollIntoView({ behavior: "smooth" });
+          }
+        });
+      });
+    });
+  
